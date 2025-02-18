d8e37fdf2fb4225507fa57940ceed997b93c376b
Robert Legge <rglegge2@gmail.com>
2025-02-18 13:01:36 -0500

    Update javadoc

--- sdk/documentintelligence/azure-ai-documentintelligence/src/main/java/com/azure/ai/documentintelligence/implementation/DocumentIntelligenceClientImpl.java
+++ sdk/documentintelligence/azure-ai-documentintelligence/src/main/java/com/azure/ai/documentintelligence/implementation/DocumentIntelligenceClientImpl.java
@@ -761,7 +761,7 @@ public final class DocumentIntelligenceClientImpl {
      * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
      * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
      * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
-     * @return the generated searchable PDF output from document analysis along with {@link Response} on successful
+     * @return the generated output from document analysis along with {@link Response} on successful
      * completion of {@link Mono}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
@@ -789,7 +789,7 @@ public final class DocumentIntelligenceClientImpl {
      * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
      * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
      * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
-     * @return the generated searchable PDF output from document analysis along with {@link Response}.
+     * @return the generated output from document analysis along with {@link Response}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public Response<BinaryData> getAnalyzeResultWithResponse(String modelId, String resultId,
